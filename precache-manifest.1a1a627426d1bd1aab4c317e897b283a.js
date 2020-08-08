77aead561d3c88a92ea7e97d337934de1b0f4339
sunil <sunilkumarbajpai.skb@gmail.com>
2020-08-08 17:56:28 +0530

    Updates

---
+++ precache-manifest.1a1a627426d1bd1aab4c317e897b283a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe691be0e6e9746baa6fd8ad16986a82",
+    "url": "/Tooltip-React-project/index.html"
+  },
+  {
+    "revision": "c00305937c70ac7fcd0a",
+    "url": "/Tooltip-React-project/static/css/main.c99228a5.chunk.css"
+  },
+  {
+    "revision": "20e2ba176bf6cbb11c2d",
+    "url": "/Tooltip-React-project/static/js/2.6bc5b324.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Tooltip-React-project/static/js/2.6bc5b324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c00305937c70ac7fcd0a",
+    "url": "/Tooltip-React-project/static/js/main.26ba7fbc.chunk.js"
+  },
+  {
+    "revision": "f4a02a5cd08ce7b4c352",
+    "url": "/Tooltip-React-project/static/js/runtime-main.56f9e284.js"
+  }
+]);
